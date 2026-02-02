4b7b66304bca16f99233c6768b6c3c7983d5ea77
Your Name <you@example.com>
2026-02-01 18:33:39 -0700

    implemented the matches logic

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import {matchRouter} from "./routes/matches.js";
 
 const app = express();
 const port = 8000;
@@ -8,6 +9,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Welcome to the Broadcast Server!');
 });
+
+app.use('/matches', matchRouter)
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
